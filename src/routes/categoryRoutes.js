098fd9a842d3841f2ac9fc3064ad6281dd02fca9
yadukrishnan <yadhu4507@gmail.com>
2025-06-05 15:11:16 +0530

    create category functionality added

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,5 +1,8 @@
 import express from "express";
+import createCategory from "../category/createCategory.js";
 
 const router = express.Router();
+
+router.post("/create", createCategory);
 
 export default router;
