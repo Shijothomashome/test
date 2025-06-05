3d67528197039f0d1a902716e1d1d8feb90af6e5
yadukrishnan <yadhu4507@gmail.com>
2025-06-05 18:43:50 +0530

    add get categorys

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -3,6 +3,7 @@ import createCategory from "../category/createCategory.js";
 import updateCategory from "../category/updateCategory.js";
 import updateToggleStatus from "../category/updateTogglestatus.js";
 import deleteCategory from "../category/deleteCategory.js";
+import categoryController from "../category/getCategory.js";
 const router = express.Router();
 
 //admin routers
@@ -10,8 +11,11 @@ router.post("/", createCategory);// create new category
 router.put("/:id", updateCategory);// update category
 router.patch("/:id/toggle-status", updateToggleStatus);// update status acitve
 router.delete("/:id", deleteCategory);// delete category
+router.get('/',categoryController.getAllCategories)
 
-//user
+
+//user routes
+router.get('/',categoryController.getSubandParentCategories)
 
 
 export default router;
