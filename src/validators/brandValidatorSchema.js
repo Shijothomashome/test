2e94813189ccc03085606ef7496238a9937100f6
shijo thomas <shijothomashome@gmail.com>
2025-06-10 19:13:36 +0530

    Add structure modifications

---
+++ src/validators/brandValidatorSchema.js
@@ -0,0 +1,10 @@
+import Joi from 'joi';
+import mongoose from 'mongoose';
+
+const isValidObjectId = (value, helpers) => {
+  if (!mongoose.Types.ObjectId.isValid(value)) {
+    return helpers.error("any.invalid");
+  }
+  return value;
+};
+
