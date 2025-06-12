992c5e35ad89de58949ee405069402c8fad83fc4
yadukrishnan <yadhu4507@gmail.com>
2025-06-12 16:55:56 +0530

    update wishlist

--- src/routes/wishlistRoutes.js
+++ src/routes/wishlistRoutes.js
@@ -5,5 +5,5 @@ import whishlistValidator from "../validators/whishlistValidator.js";
 const router = express.Router();
 router.post('/',middlewares.validatorMiddleware(whishlistValidator.wishlistItemValidator), wishlistController.createWishlist);
 router.get('/',wishlistController.getWishlist);
-router.put('/:userId',wishlistController.removeWishlistItem)
+// router.put('/:userId',wishlistController.removeWishlistItem)
 export default router;
