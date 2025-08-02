829dcda5c586c0f891e93c080f8e24785b315eb5
adarshjithu <adarshjithu10@gmail.com>
2025-08-02 16:58:36 +0530

    Update google callback frontend url

--- src/api/user/product/getProductByCategoryId.js
+++ src/api/user/product/getProductByCategoryId.js
@@ -3,43 +3,57 @@ import { BadRequestError } from "../../../constants/customErrors.js";
 import productModel from "../../../models/productModel.js";
 
 export const getProductsByCategoryId = async (req, res, next) => {
-    try {
-        const { categoryId } = req.params;
-        const { limit } = req.query;
-        if (!mongoose.Types.ObjectId.isValid(categoryId)) throw new BadRequestError("Invalid category Id");
-        const products = await productModel.aggregate([
-            { $match: { category: new mongoose.Types.ObjectId(categoryId) } },
-            {
-                $addFields: {
-                    offer: {
-                        $round: [
-                            {
-                                $multiply: [
-                                    {
-                                        $divide: [{ $subtract: ["$basePrice.mrp", "$basePrice.sellingPrice"] }, "$basePrice.mrp"],
-                                    },
-                                    100,
-                                ],
-                            },
-                            0,
-                        ],
-                    },
-                },
-            },
-            {
-                $project: {
-                    name: 1,
-                    description: 1,
-                    image: "$thumbnail",
-                    basePrice: 1,
-                    offer: 1,
-                },
-            },
-            { $limit: limit || 5 },
-        ]);
+  try {
+    const { categoryId } = req.params;
+    console.log(categoryId)
+    const { limit } = req.query;
 
-        res.status(200).json({ success: true, message: "Product data fetched successfully", data: products });
-    } catch (error) {
-        next(error);
+    if (!mongoose.Types.ObjectId.isValid(categoryId)) {
+      throw new BadRequestError("Invalid category Id");
     }
+
+    const limitNumber = parseInt(limit) || 5;
+
+    const products = await productModel.aggregate([
+      { $match: { category: new mongoose.Types.ObjectId(categoryId) } },
+      {
+        $addFields: {
+          offer: {
+            $round: [
+              {
+                $multiply: [
+                  {
+                    $divide: [
+                      { $subtract: ["$basePrice.mrp", "$basePrice.sellingPrice"] },
+                      "$basePrice.mrp",
+                    ],
+                  },
+                  100,
+                ],
+              },
+              0,
+            ],
+          },
+        },
+      },
+      {
+        $project: {
+          name: 1,
+          description: 1,
+          image: "$thumbnail",
+          basePrice: 1,
+          offer: 1,
+        },
+      },
+      { $limit: limitNumber },
+    ]);
+
+    res.status(200).json({
+      success: true,
+      message: "Product data fetched successfully",
+      data: products,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,29 +1,36 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import passport from 'passport';
-import middlewares from "../middlewares/index.js"
-import userValidatorSchemas from '../validators/index.js';
+import passport from "passport";
+import middlewares from "../middlewares/index.js";
+import userValidatorSchemas from "../validators/index.js";
 import authControllers from "../controllers/auth/index.js";
-import testResponse from '../utils/testResponseUtils.js';
-
+import testResponse from "../utils/testResponseUtils.js";
 
 // TEST Protected Route
-router.get('/protected', middlewares.authenticate(['customer', 'admin']), testResponse);
+router.get("/protected", middlewares.authenticate(["customer", "admin"]), testResponse);
 
 // GOOGLE AUTH
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email',] }));
-router.get('/google/callback', passport.authenticate('google', { session: false, failureRedirect: 'api/v1/auth/google/failure', }), authControllers.googleCallback);
-router.get('/google/failure',authControllers.googleFailureLogin);
+router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+router.get(
+    "/google/callback",
+    passport.authenticate("google", { session: false, failureRedirect: "api/v1/auth/google/failure" }),
+    authControllers.googleCallback
+);
+router.get("/google/failure", authControllers.googleFailureLogin);
 
 // REGISTER AND LOGIN
-router.post('/customer/register', middlewares.validatorMiddleware(userValidatorSchemas.userSchema), authControllers.customerRegister);
-router.post('/customer/login', middlewares.validatorMiddleware(userValidatorSchemas.loginSchema), authControllers.customerLogin);
-router.post('/customer/otp-login', authControllers.customerOTPLogin);
-router.post('/otp/send', authControllers.sendOTP);
-router.post('/otp/verify', authControllers.verifyOTP);
-router.post('/customer/password-reset', middlewares.validatorMiddleware(userValidatorSchemas.passwordResetSchema), authControllers.customerPasswordReset);
+router.post("/customer/register", middlewares.validatorMiddleware(userValidatorSchemas.userSchema), authControllers.customerRegister);
+router.post("/customer/login", middlewares.validatorMiddleware(userValidatorSchemas.loginSchema), authControllers.customerLogin);
+router.post("/customer/otp-login", authControllers.customerOTPLogin);
+router.post("/otp/send", authControllers.sendOTP);
+router.post("/otp/verify", authControllers.verifyOTP);
+router.post(
+    "/customer/password-reset",
+    middlewares.validatorMiddleware(userValidatorSchemas.passwordResetSchema),
+    authControllers.customerPasswordReset
+);
 router.get("/regenerate-accessToken", authControllers.regenerateAccessToken);
-router.get("/logout", middlewares.authenticate(['customer', 'admin']), authControllers.logout);
-router.get("/me", middlewares.authenticate(['customer', 'admin']), authControllers.me);
+router.get("/logout", middlewares.authenticate(["customer", "admin"]), authControllers.logout);
+router.get("/me", middlewares.authenticate(["customer", "admin"]), authControllers.me);
 
 export default router;
