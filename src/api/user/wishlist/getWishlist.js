45f6ed2a011af30b27d852d83978b147f699f9b2
adarshjithu <adarshjithu10@gmail.com>
2025-08-03 12:36:16 +0530

    Add filter in wishlist page

--- src/api/user/wishlist/getWishlist.js
+++ src/api/user/wishlist/getWishlist.js
@@ -1,12 +1,27 @@
 import mongoose from "mongoose";
 import wishlistModel from "../../../models/wishlistModel.js";
+import { BadRequestError } from "../../../constants/customErrors.js";
 
 export const getWishlist = async (req, res, next) => {
     try {
-        const { page = 1, limit = 15 } = req.query;
+        const { page = 1, limit = 15,parent_id='' } = req.query;
         const userObjectId = new mongoose.Types.ObjectId(req.user?._id);
 
         const skip = (parseInt(page) - 1) * parseInt(limit);
+
+        const categoryCheckMatchStage = {}
+
+        
+            if(parent_id&&!mongoose.Types.ObjectId.isValid(parent_id)){
+                throw new BadRequestError("Invalid parent category Id")
+            }
+            if(parent_id){
+                categoryCheckMatchStage.$match={category:new mongoose.Types.ObjectId(parent_id)}
+            }else{
+                categoryCheckMatchStage.$match={}
+            }
+        
+       
 
         const products = await wishlistModel.aggregate([
             { $match: { userId: userObjectId } },
@@ -22,8 +37,9 @@ export const getWishlist = async (req, res, next) => {
                     basePrice: "$productData.basePrice",
                     _id: "$productData._id",
                     image: "$productData.thumbnail",
+                    category:"$productData.category"
                 },
-            },
+            },categoryCheckMatchStage,
             {
                 $addFields: {
                     offer: {
@@ -49,6 +65,8 @@ export const getWishlist = async (req, res, next) => {
             { $limit: parseInt(limit) },
         ]);
 
+
+        
         res.status(200).json({
             success: true,
             data: products,
